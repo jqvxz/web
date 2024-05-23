b8e1b8c9d9e37f89932ea3a86dad5a77df3c523b
Javon <143961758+jqvxz@users.noreply.github.com>
2024-05-23 21:52:14 +0200

    Delete js directory

--- js/opera.js
+++
@@ -1,5 +0,0 @@
-// Ban opera gx users
-  var isOperaGX = navigator.userAgent.includes("OPR") && navigator.userAgent.includes("Opera GX");
-  if (isOperaGX) {
-    window.location.href = "opera.html";
-  }
